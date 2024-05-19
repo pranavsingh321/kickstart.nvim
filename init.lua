b4aadf9a2732bd0ba0ed9ab1c61afa7bb7cccfc2
pranav <pranavsingh321@gmail.com>
2024-05-19 23:49:47 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -921,3 +921,9 @@ autocmd('BufReadPost', {
     end
   end,
 })
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
