3fbe1716dfca86c12e26f5691f09bed92e70739e
pranav <pranavsingh321@gmail.com>
2024-05-21 14:45:07 +0200

    remove trailing spaces

--- init.lua
+++ init.lua
@@ -928,3 +928,5 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.api.nvim_set_keymap('n', '<CR>', 'o<Esc>', { noremap = true })
+-- Remove trailing whitespace on save
+vim.cmd [[autocmd BufWritePre * %s/\s\+$//e]]
